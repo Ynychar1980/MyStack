7174e18b4e93514dea9748e828dd632fd4bc4175
Ynychar1980 <yanychar1980@gmail.com>
2021-06-21 13:04:00 +0300

    Добавьте файлы проекта.

---
+++ Model/ArrayStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack.Model
+{
+   public class ArrayStack<T>
+   {
+      T[] items;
+      private int Current = -1;
+      private readonly int size = 10;
+
+      public int MaxCount => items.Length;
+      public int Count => Current + 1;
+
+      public ArrayStack(int size = 10)
+      {
+         items = new T[size];
+         this.size = size;
+      }
+
+      public ArrayStack(T data, int size = 10) : this(size)
+      {
+         items[0] = data;
+         Current = 1;
+      }
+
+      public void Push(T data)
+      {
+         if(Current < size - 1)
+         {
+            Current++;
+            items[Current] = data;            
+         }
+         else
+         {
+            throw new StackOverflowException("Стек переполнен");
+         }
+      }
+
+      public T Pop()
+      {
+         if(Current >= 0)
+         {
+            var item = items[Current];
+            Current--;
+            return item;
+         }
+         else
+         {
+            throw new NullReferenceException("Стек пуст");
+         }
+      }
+
+      public T Peek()
+      {
+         if (Current >= 0)
+         {
+            return items[Current];
+         }
+         else
+         {
+            throw new NullReferenceException("Стек пуст");
+         }
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using MyStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         EasyStack<int> easyStack = new EasyStack<int>();
+
+         easyStack.Push(1);
+         easyStack.Push(2);
+         easyStack.Push(3);
+         easyStack.Push(4);
+         easyStack.Push(5);
+
+         var item1 = easyStack.Pop();
+         var item2 = easyStack.Peek();
+
+         Console.WriteLine(item1);
+         Console.WriteLine(item2);
+         Console.ReadLine();
+
+
+         var linkedStack = new LinkedStack<int>();
+         linkedStack.Push(10);
+         linkedStack.Push(20);
+         linkedStack.Push(30);
+         linkedStack.Push(40);
+         linkedStack.Push(50);
+
+         Console.WriteLine(linkedStack.Peek());
+         Console.WriteLine(linkedStack.Pop());
+         Console.WriteLine(linkedStack.Pop());
+         Console.WriteLine(linkedStack.Peek());
+         Console.ReadLine();
+
+         var arrayStack = new ArrayStack<int>(5);
+         arrayStack.Push(100);
+         arrayStack.Push(200);
+         arrayStack.Push(300);
+         arrayStack.Push(400);
+         arrayStack.Push(500);
+
+         Console.WriteLine(arrayStack.Peek());
+         Console.WriteLine(arrayStack.Pop());
+         Console.WriteLine(arrayStack.Pop());
+         Console.WriteLine(arrayStack.Peek());
+
+         Console.ReadLine();
+      }
+   }
+}
